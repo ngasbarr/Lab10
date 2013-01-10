f6eeb234d4f33ce26cd29d4e3d6f5b5dd43651ab
NickG <ngasbarr@andrew.cmu.edu>
2013-01-09 23:53:59 -0500

    finished up to the evening section part

--- city_pop.rb
+++ city_pop.rb
@@ -4,3 +4,38 @@ def get_population(state, *file)
 	cities = {}
 	pop_file = File.open(filename)
 	
+	pop_file.each_line do |line|
+		city, st, population = line.split(/\t/)
+		if state == st
+			cities[city] = population.chomp
+		end
+	end
+	
+	puts "Top Cities in #{state}"
+	puts "----------------------"
+	cities.sort.each do |city, population|
+		puts "#{city} (#{population})"
+	end
+end
+
+#get_population("CA", "citypop.txt")
+#puts
+#get_population("PA")
+
+class Population
+	def initialize(*file)
+		@filename = (file[0] ||= 'citypop.txt' )
+	end
+	
+	def get_pop_for(state)
+		get_population(state, @filename)
+	end
+end
+
+test = Population.new
+test.get_pop_for("TX")
+puts
+test.get_pop_for("CA")
+puts
+test2 = Population.new("citypop_1900.txt")
+test2.get_pop_for("NY")
