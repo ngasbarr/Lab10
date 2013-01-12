d006c64f8cbc1f115c8ae70e0c58e36279da54c3
NickG <ngasbarr@andrew.cmu.edu>
2013-01-12 00:22:39 -0500

    finished the more efficient code

--- city_pop.rb
+++ city_pop.rb
@@ -1,22 +1,4 @@
-def get_population(state, *file)
-	filename = ( file[0] ||= 'citypop.txt' )
-	
-	cities = {}
-	pop_file = File.open(filename)
-	
-	pop_file.each_line do |line|
-		city, st, population = line.split(/\t/)
-		if state == st
-			cities[city] = population.chomp
-		end
-	end
-	
-	puts "Top Cities in #{state}"
-	puts "----------------------"
-	cities.sort.each do |city, population|
-		puts "#{city} (#{population})"
-	end
-end
+
 
 #get_population("CA", "citypop.txt")
 #puts
@@ -25,10 +7,38 @@ end
 class Population
 	def initialize(*file)
 		@filename = (file[0] ||= 'citypop.txt' )
+		@file_lines = Array.new
+	end
+	
+	def read_file
+		if @file_lines.empty?
+			@file = File.open(@filename)
+			@file_lines = @file.readlines()
+		end
+	end
+	
+	def filename(new_file)
+		@filename = new_file
+		@file_lines = Array.new
 	end
 	
 	def get_pop_for(state)
-		get_population(state, @filename)
+		read_file()
+		cities = {}
+		
+		@file_lines.each do |line|
+			city, st, population = line.split(/\t/)
+			if state == st
+				cities[city] = population.chomp
+			end
+		end
+		
+		puts "Top Cities in #{state}"
+		puts "----------------------"
+		cities.sort.each do |city, population|
+			puts "#{city} (#{population})"
+		end
+		
 	end
 end
 
@@ -36,6 +46,6 @@ test = Population.new
 test.get_pop_for("TX")
 puts
 test.get_pop_for("CA")
-puts
+puts "TEST 2"
 test2 = Population.new("citypop_1900.txt")
 test2.get_pop_for("NY")
